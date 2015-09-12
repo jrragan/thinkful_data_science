1833654cc288a6f94707b99d2f48112a91020553
James Ragan <yellowjaket0694@hotmail.com>
2015-09-12 03:30:39 -0400

    Challenge Script for Unit 2 Lesson 2 Testing Loan Data

---
+++ chi-squared.py
@@ -0,0 +1,23 @@
+__author__ = 'rragan'
+
+import collections
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import stats
+
+# Load the reduced version of the Lending Club Dataset
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+# Drop null rows
+loansData.dropna(inplace=True)
+
+freq = collections.Counter(loansData['Open.CREDIT.Lines'])
+
+plt.figure()
+plt.bar(freq.keys(), freq.values(), width=1)
+plt.show()
+
+chi, p = stats.chisquare(freq.values())
+
+print(chi)
+print(p)
